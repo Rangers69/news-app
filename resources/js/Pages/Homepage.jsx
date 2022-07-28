93d637e33cc1cf12151318f8c41089534ed9b7b9
Rangers69 <ahmadwidodo89@gmail.com>
2022-07-28 16:17:43 +0700

    laravel+react.js

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+
+export default function Homepage(props){
+    console.log(props);
+    return (
+        <div className='flex justify-center items-center min-h-screen bg-neutral-800 text-white text-2xl'>
+            <Head title={props.title} />
+            <h1>{props.description}</h1>
+        </div>
+    );
+}
